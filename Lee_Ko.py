d3a843e1b271b731ce6c11c3f3ebc885a9abba7b
s_Yoon <s_ksyoon@yulchon.com>
2026-03-27 18:32:32 +0900

    Fix: 광장 실험용 코드로 변경

--- Lee_Ko.py
+++ Lee_Ko.py
@@ -53,7 +53,8 @@ time.sleep(1)
 company = "광장"
 
 categories = driver.find_elements(By.XPATH, '//*[@id="mCSB_2_container"]/li')
-for category in tqdm.tqdm(range(2, len(categories)+1)):
+# for category in tqdm.tqdm(range(2, len(categories)+1)):
+for category in tqdm.tqdm(range(2, 3)):
     pf_data = []
     # 카테고리 선택
     category_box = driver.find_element(By.XPATH, "//div[@class='leeko-member-search__select']//div[@class='nice-select chosen-select']")
@@ -65,15 +66,15 @@ for category in tqdm.tqdm(range(2, len(categories)+1)):
     driver.find_element(By.XPATH, "//div[@class='leeko-member-search__form']/button").click() # 검색 버튼 클릭
     time.sleep(3)
 
-    # 모든 더보기 버튼 클릭해서 화면에 pf 정보가 다 뜨도록 설정
-    while True:
-        try:
-            button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[6]/a/strong')
-            driver.execute_script("arguments[0].scrollIntoView({block: 'nearest'});", button)
-            time.sleep(1)
-            button.click()
-        except:
-            break
+    # # 모든 더보기 버튼 클릭해서 화면에 pf 정보가 다 뜨도록 설정
+    # while True:
+    #     try:
+    #         button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[6]/a/strong')
+    #         driver.execute_script("arguments[0].scrollIntoView({block: 'nearest'});", button)
+    #         time.sleep(1)
+    #         button.click()
+    #     except:
+    #         break
 
     category_member_lst = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[5]/div') # 구성원 리스트
     pf_lines = category_member_lst.find_elements(By.CSS_SELECTOR, "div.leeko-member__list") # 구성원 리스트에서 한 줄씩 뽑기
