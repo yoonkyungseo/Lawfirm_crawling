f3742e708a29df669d163ac4d580411dbcc905a8
s_Yoon <s_ksyoon@yulchon.com>
2026-04-01 13:13:07 +0900

    Fix: 김앤장 performance 부분 수정

--- Kim_and_Chang.py
+++ Kim_and_Chang.py
@@ -226,7 +226,7 @@ for num in tqdm.tqdm(range(1, len(elements)+1)):
                         # 주요 업무 실적
                         elif "주요 실적" in main_activity.get_attribute("textContent"):
                             print("주요 실적")
-                            perf_lst = wait_presence_elements(extra, (By.CSS_SELECTOR, ' div.boxopen *'))
+                            perf_lst = wait_presence_elements(extra, (By.XPATH, './/div[@class="box_open"]//*'))
                             print(len(perf_lst))
                             imsi_tit = []
                             imsi_cont = ""
