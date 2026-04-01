662e7e507cd08662249c977a10890036089a56f7
s_Yoon <s_ksyoon@yulchon.com>
2026-04-01 17:47:50 +0900

    Fix: 김앤장 eligibility 오류 수정 중

--- Kim_and_Chang.py
+++ Kim_and_Chang.py
@@ -169,8 +169,7 @@ for num in tqdm.tqdm(range(2, len(elements)+1)):
                     # 자격
                     try:
                         eli_lst = wait_presence_elements(driver, (By.XPATH, '//*[@id="career"]/*[@class="bullet_list"][3]//*'))
-                        print(len(eli_lst))
-                        if eli_lst[1].tag_name == "ul":
+                        if eli_lst[0].tag_name == "ul":
                             eli_lst = wait_presence_elements(eli_lst[1], (By.XPATH, './/*'))
                         eli_total = []
                         for elis in eli_lst:
