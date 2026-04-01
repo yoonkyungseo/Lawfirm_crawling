466291738bd237fc8a5aa6371f7572bec6784535
s_Yoon <s_ksyoon@yulchon.com>
2026-04-01 14:19:12 +0900

    Fix: 김앤장 performance 수집 장현주 변호사 확인용

--- Kim_and_Chang.py
+++ Kim_and_Chang.py
@@ -238,7 +238,7 @@ for num in tqdm.tqdm(range(1, len(elements)+1)):
                             imsi_cont = ""
                             imsi_cont_lst = []
                             for perf in perf_lst:
-                                print(perf.tag_name)
+                                print(perf.tag_name, perf.get_attribute("textContent"))
                                 if perf.tag_name == 'strong':
                                     txt_tit = perf.get_attribute("textContent").replace("[","").replace("]","").strip()
                                     if txt_tit:
