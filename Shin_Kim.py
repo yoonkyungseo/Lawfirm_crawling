83334b1d6b1568dffeb4187bf17b233190a9920a
yoonkyungseo <rudtj0107@gmail.com>
2026-04-10 00:38:28 +0900

    Fix: 세종 pagination 부분 코드 오류 수정

--- Shin_Kim.py
+++ Shin_Kim.py
@@ -280,7 +280,8 @@ for category in tqdm.tqdm(range(2, len(categories)+1)):
                 page_num += 1
                 pagination = driver.find_element(By.CSS_SELECTOR, 'span.pagination')
                 driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", pagination)
-                click_page = pagination.find_element(By.XPATH, f'.//a[{page_num}]')
+                click_page = pagination.find_element(By.XPATH, f'.//a[normalize-space()="{page_num}"]')
+                print(f"{click_page.get_attribute("textContent")}페이지를 클릭합니다.")
                 driver.execute_script("arguments[0].click();", click_page)
                 time.sleep(2)
             else:
