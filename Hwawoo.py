ef997d593a093366bbc34705d11a6c059cfdf1aa
s_Yoon <s_ksyoon@yulchon.com>
2026-06-02 13:25:22 +0900

    Fix: 화우 introduction 오류 수정

--- Hwawoo.py
+++ Hwawoo.py
@@ -109,7 +109,7 @@ for i in tqdm.tqdm(range(1, len(pf_lst)+1)):
         
         # 관련분야, 경력, 학력, 자격, 수상, 주요업무실적
         detail_table = driver.find_elements(By.CSS_SELECTOR, ' div.tab-wrap.box-detail-contents.tab-ctrl > article[class^="dtail"]')
-        eligibility, awards, performance, activity = "", "", "", ""
+        introduction, eligibility, awards, performance, activity = "", "", "", "", ""
         for detail in detail_table:
             detail_title = detail.find_element(By.CSS_SELECTOR, 'h3').get_attribute("textContent").strip()
             
